9367ea1d4ec53e2af24f25d839edbdc4353c15ce
Andrii <andrii.khomik@gmail.com>
2024-09-30 14:47:22 +0300

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,22 +2,26 @@ variable "project_id" {
   description = "Project id"
   type        = string
   sensitive   = true
+  default     = "ferrous-amphora-435308-b9"
 }
 
 variable "region" {
   description = "Region"
   type        = string
   sensitive   = true
+  default     = "us-central1"
 }
 
 variable "user" {
   description = "User"
   type        = string
+  default     = "andrii_khomik"
 }
 
 variable "email" {
   description = "Email"
   type        = string
+  default     = "terrafofm-sa@ferrous-amphora-435308-b9.iam.gserviceaccount.com"
 }
 
 variable "prefix" {
