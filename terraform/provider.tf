aa8314c685a7e53b6edf749ff0161f08c25feca3
Andrii <andrii.khomik@gmail.com>
2024-10-09 09:37:20 +0300

    comment out credentials

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_id
-  credentials = file("gcp-credentials.json")
-  region      = var.region
-  zone        = "${var.region}-c"
+  project = var.project_id
+  # credentials = file("gcp-credentials.json")
+  region = var.region
+  zone   = "${var.region}-c"
 }
